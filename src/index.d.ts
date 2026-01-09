d19a465fcaefcb3c09f15c3072a865a969b89c86
SpireX <SpireX@outlook.com>
2026-01-09 17:57:34 +0200

    feat: Add types for task & process

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,19 @@
 export declare type TRunnable = () => void | Promise<void>;
 
-export declare function createBootTask(name: string, runnable: TRunnable): void;
+export type TBootTask = {
+    readonly name;
+    readonly run: TRunnable;
+};
 
-export interface IBootProcess {}
+export declare function createBootTask(
+    name: string,
+    runnable: TRunnable,
+): TBootTask;
+
+export interface IBootProcess {
+    readonly count: number;
+
+    add(task: TBootTask): IBootProcess;
+}
 
 export declare function createBootProcess(): IBootProcess;
